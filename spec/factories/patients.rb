fbabb6ed6b8204b3ccbd0dd0897cc46cf5fdf236
Lia Skalkos <hello@liaskalkos.net>
2015-08-03 17:33:54 -0400

    Creates doctor and patient login feature tests

---
+++ spec/factories/patients.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :patient do
+    first_name "Bowser"
+    last_name  "Koopa"
+    email "patientbowser@nintendo.com"
+    password "password"
+    phone_number "330-503-6782"
+  end
+end
---
+++ spec/features/doctor_login_spec.rb
@@ -0,0 +1,57 @@
+require 'feature_helper'
+
+feature 'Doctor logs in from the homepage and' do
+
+  let(:doctor) {create(:doctor)}
+  let(:patient) {create(:patient)}
+
+
+  scenario 'they log in successfully' do
+    visit doctor_login_path
+
+    fill_in 'Email', with: doctor.email
+    fill_in 'Password', with: doctor.password
+
+    click_button 'Sign In'
+
+    expect(page.current_path).to eq "/doctors/#{doctor.id}/patients"
+    expect(page).to have_content("Hello Dr. #{doctor.last_name}.")
+  end
+
+  scenario 'they enter the wrong password' do
+    visit doctor_login_path
+
+    fill_in 'Email', with: doctor.email
+    fill_in 'Password', with: 'password1234blah'
+
+    click_button 'Sign In'
+
+    expect(page.current_path).to eq doctor_login_path
+    expect(page).to have_content("Invalid email or password.")
+  end
+
+  scenario 'they are not a valid user' do
+    visit doctor_login_path
+
+    fill_in 'Email', with: 'doctorwario@nintendo.com'
+    fill_in 'Password', with: 'password1234blah'
+
+    click_button 'Sign In'
+
+    expect(page.current_path).to eq doctor_login_path
+    expect(page).to have_content("Invalid email or password.")
+  end
+
+  scenario 'they are not able to log in if they are a patient' do
+    visit doctor_login_path
+
+    fill_in 'Email', with: patient.email
+    fill_in 'Password', with: patient.password
+
+    click_button 'Sign In'
+
+    expect(page.current_path).to eq doctor_login_path
+    expect(page).to have_content("Invalid email or password.")
+  end
+
+end
