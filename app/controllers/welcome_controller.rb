428477f0d77c066e4776abc1ade5e8b3d764e477
lizlove <elizabethlovero@gmail.com>
2015-08-04 16:02:22 -0400

    patient and doctor login sucess

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
    def index
-      binding.pry
+      # binding.pry
    end 
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -7,8 +7,8 @@ has_many :doctor_patients
 has_many :patients, through: :doctor_patients
 
 #do we need to have password_confirmation here?
-has_secure_password
+# has_secure_password
 
-validates :first_name, :last_name, :email, :phone_number, :password, presence: true
+# validates :first_name, :last_name, :email, :phone_number, :password, presence: true
 
 end
