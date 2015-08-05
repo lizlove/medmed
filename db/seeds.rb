6aeeb324cf6692a420af7afa3ffd65b899b2097c
lizlove <elizabethlovero@gmail.com>
2015-08-05 13:06:55 -0400

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 
+# Patients
 
 
-
+ 
+# Doctors
+ #<ActiveRecord::Relation [#<Doctor id: 1, first_name: "Jumpman", last_name: "Mario", phone_number: "329-324-2343", email: "drmario@nintendo.com", encrypted_password: "$2a$10$3tSDjs8Udzoz/VHNFk790.9hu8ceDmG051T82Hovokp...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-08-05 15:35:43", last_sign_in_at: "2015-08-05 15:35:43", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1">, #<Doctor id: 2, first_name: "Toad", last_name: "Mushroom", phone_number: "897-098-3425", email: "toad@nintendo.com", encrypted_password: "$2a$10$qjEPhDhejElrqp/iKcwhaOwE3X/CszhLlKPiUQn.gmY...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 7, current_sign_in_at: "2015-08-05 15:59:43", last_sign_in_at: "2015-08-05 15:59:18", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1">]> 
 
 
