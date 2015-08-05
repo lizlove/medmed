b00540a31a5e6c6db10a6a0ee08513c6271cd869
lizlove <elizabethlovero@gmail.com>
2015-08-05 14:29:53 -0400

    patient view prelim done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@ ScheduledDose.destroy_all
 
 # Patients
 @patient1 = Patient.create!(first_name: "Princess", last_name: "Peach", phone_number: "804-596-0982", email: "peach@nintendo.com", password: "marioh0t", password_confirmation: "marioh0t")
-Patient.create!(first_name: "Bowser", last_name: "Koopa", phone_number: "804-596-0952", email: "koopa@nintendo.com", password: "kooparules", password_confirmation: "kooparules")
+@patient2 = Patient.create!(first_name: "Bowser", last_name: "Koopa", phone_number: "804-596-0952", email: "koopa@nintendo.com", password: "kooparules", password_confirmation: "kooparules")
 
 # Doctors
 @doctor1 = Doctor.create!(first_name: "Jumpman", last_name: "Mario", phone_number: "329-324-2343", email: "drmario@nintendo.com", password: "wariosux", password_confirmation: "wariosux")
-Doctor.create!(first_name: "Toad", last_name: "Mushroom", phone_number: "329-324-2393", email: "toad@nintendo.com", password: "toadrules", password_confirmation: "toadrules")
+@doctor2 = Doctor.create!(first_name: "Toad", last_name: "Mushroom", phone_number: "329-324-2393", email: "toad@nintendo.com", password: "toadrules", password_confirmation: "toadrules")
 
 #Prescription
 @prescrip1 = Prescription.new(patient_id: @patient1.id, rxcui: 541894, start_datetime: Time.now, end_datetime: Time.now + 5.days, doctor_id: @doctor1.id, side_effects: nil, image_url: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg", medication_name: "Dextroamphetamine saccharate 2.5 MG")
@@ -28,7 +28,7 @@ Doctor.create!(first_name: "Toad", last_name: "Mushroom", phone_number: "329-324
 @prescrip3.add_daily_recurrence_rule(1)
 @prescrip3.save
 
-@prescrip4 = Prescription.new(patient_id: @patient1.id, rxcui: 617314, start_datetime: Time.now, end_datetime: Time.now + 14.days, doctor_id: @doctor1.id, side_effects: nil, image_url: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg", medication_name: "atorvastatin 10 MG Oral Tablet [Lipitor]")
+@prescrip4 = Prescription.new(patient_id: @patient2.id, rxcui: 617314, start_datetime: Time.now, end_datetime: Time.now + 14.days, doctor_id: @doctor2.id, side_effects: nil, image_url: "http://pillbox.nlm.nih.gov/assets/small/540920173.jpg", medication_name: "atorvastatin 10 MG Oral Tablet [Lipitor]")
 
 @prescrip4.add_daily_recurrence_rule(2)
 @prescrip4.save
