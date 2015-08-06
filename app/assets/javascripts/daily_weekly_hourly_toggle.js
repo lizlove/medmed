f738b9882fa04aea0d90b64e741f44ecd5b46e82
benbrattain <ben.brattain@gmail.com>
2015-08-06 14:19:09 -0400

    prescriptions can be created and generate their scheduled doses.

--- app/assets/javascripts/daily_weekly_hourly_toggle.js
+++ app/assets/javascripts/daily_weekly_hourly_toggle.js
@@ -2,18 +2,12 @@ $(document).ready(function(){
   $('select.recurrence_option').on('change', function(){
     if ("Daily" == $(this).val()){
       $(".field_to_switch :nth-child(3)").text("days")
-      $(".field_to_switch :nth-child(2)").attr("name", "days")
-      $(".field_to_switch :nth-child(2)").attr("id", "days")
       $(".weekdays").hide()
     } else if ("Hourly" == $(this).val()){
       $(".field_to_switch :nth-child(3)").text("hours")
-      $(".field_to_switch :nth-child(2)").attr("name", "hours")
-      $(".field_to_switch :nth-child(2)").attr("id", "hours")
       $(".weekdays").hide()
     } else if("Weekly" == $(this).val()){
       $(".field_to_switch :nth-child(3)").text("weeks")
-      $(".field_to_switch :nth-child(2)").attr("name", "weeks")
-      $(".field_to_switch :nth-child(2)").attr("id", "weeks")
       $(".weekdays").show()
     }
   })
