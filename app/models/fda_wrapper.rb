abd7a0ceab5051b1763842f60cfdc0b88c0a47f9
lizlove <elizabethlovero@gmail.com>
2015-08-06 17:07:37 -0400

    resetting figaro

--- app/models/fda_wrapper.rb
+++ app/models/fda_wrapper.rb
@@ -2,10 +2,10 @@ class FdaWrapper
 
   FDA_KEY = ENV["fda_key"]
 
-  def self.request(rxcui)
+  def request(rxcui)
+    binding.pry
     base_url = "https://api.fda.gov/drug/label.json?api_key=" + FDA_KEY + "&search=rxcui:" 
     api_request = base_url + rxcui
-    binding.pry
     api_response = open(api_request).read
     JSON.parse(api_response)["results"]
   end
