da28f97e5ee57ef21a1857e6eaab9af7d1b65703
lizlove <elizabethlovero@gmail.com>
2015-08-06 13:22:46 -0400

    first pass FDAOpen

---
+++ app/models/fdawrapper.rb
@@ -0,0 +1,14 @@
+class FdaWrapper
+
+  FDA_KEY = ENV["fda_key"]
+
+  def self.request(rxcui)
+    base_url = "https://api.fda.gov/drug/label.json?api_key=" + FDA_KEY + "&search=rxcui:" 
+    api_request = base_url + rxcui
+    api_response = open(api_request).read
+    JSON.parse(api_response)["results"]
+  end
+
+end
+
+
