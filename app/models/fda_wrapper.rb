3b6b21f12470a58485b9f8d99e886e00757cf03d
lizlove <elizabethlovero@gmail.com>
2015-08-06 18:12:14 -0400

    show page partially built, needs routes and link_to completion

--- app/models/fda_wrapper.rb
+++ app/models/fda_wrapper.rb
@@ -6,7 +6,7 @@ class FdaWrapper
     base_url = "https://api.fda.gov/drug/label.json?" + "search=rxcui:" 
     api_request = URI(URI.encode(base_url + rxcui))
     api_response = Net::HTTP.get(api_request)
-    JSON.parse(api_response)["results"]["qqspl_medguide"]
+    JSON.parse(api_response)["results"][0]["spl_medguide"][0]
   end
 
 end
