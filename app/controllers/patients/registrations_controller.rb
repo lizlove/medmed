710dfbf408c14613b71d6b3175f43f9f886fed54
lizlove <elizabethlovero@gmail.com>
2015-08-10 15:50:12 -0400

    done

--- app/controllers/patients/registrations_controller.rb
+++ app/controllers/patients/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Patients::RegistrationsController < Devise::RegistrationsController
-before_filter :configure_sign_up_params, only: [:create]
+# before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -7,30 +7,30 @@ before_filter :configure_sign_up_params, only: [:create]
   #   super
   # end
 
-  # POST /resource
-  def create
-    build_resource(sign_up_params)
-    resource.time_zone = sign_up_params[:time_zone] 
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
+  # # POST /resource
+  # def create
+  #   build_resource(sign_up_params)
+  #   resource.time_zone = sign_up_params[:time_zone] 
+  #   resource.save
+  #   yield resource if block_given?
+  #   if resource.persisted?
+  #     if resource.active_for_authentication?
+  #       set_flash_message :notice, :signed_up if is_flashing_format?
+  #       sign_up(resource_name, resource)
+  #       respond_with resource, location: after_sign_up_path_for(resource)
+  #     else
+  #       set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+  #       expire_data_after_sign_in!
+  #       respond_with resource, location: after_inactive_sign_up_path_for(resource)
+  #     end
+  #   else
+  #     clean_up_passwords resource
+  #     set_minimum_password_length
+  #     respond_with resource
+  #   end
+  # end
     
-  end
+  # end
 
   # GET /resource/edit
   # def edit
@@ -59,10 +59,9 @@ before_filter :configure_sign_up_params, only: [:create]
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :time_zone
-    devise_parameter_sanitizer.for(:sign_up) << :local
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.for(:sign_up) << :time_zone
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
