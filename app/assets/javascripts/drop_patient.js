daf86779a4f2c6294aa94ab50b924bdcfc1c80a9
Lia Skalkos <hello@liaskalkos.net>
2015-08-13 19:09:22 -0400

    Drop patient working

--- app/assets/javascripts/drop_patient.js
+++ app/assets/javascripts/drop_patient.js
@@ -1,20 +1,9 @@
 $(function(){
   $('#patient_id').on("change", function(){
     var pat_id = $('#patient_id').val();
-    $('form#delete_patient').attr("action", "/doctors/3/patients/"+pat_id);
+      var doctor_id = $('form#delete_patient').data('doctor-id');
+
+      $('form#delete_patient').attr("action", "/doctors/" + doctor_id + "/patients/"+ pat_id);
   });
 });
 
-
-//$('form#delete_patient').click(function(e){
-//    e.preventDefault();
-//
-//    var url = $(this).attr('action');
-//    var method
-//
-//
-//    $.ajax({
-//        url: url,
-//
-//    })
-//})
