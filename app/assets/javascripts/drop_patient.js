60c21f7697a0132e02269aade74ff88db44c8a1b
Lia Skalkos <hello@liaskalkos.net>
2015-08-13 18:49:45 -0400

    Some work done

--- app/assets/javascripts/drop_patient.js
+++ app/assets/javascripts/drop_patient.js
@@ -1,6 +1,20 @@
 $(function(){
   $('#patient_id').on("change", function(){
     var pat_id = $('#patient_id').val();
-    $('form#new_patient').attr("action", "/doctors/3/patients/"+pat_id);
+    $('form#delete_patient').attr("action", "/doctors/3/patients/"+pat_id);
   });
 });
+
+
+//$('form#delete_patient').click(function(e){
+//    e.preventDefault();
+//
+//    var url = $(this).attr('action');
+//    var method
+//
+//
+//    $.ajax({
+//        url: url,
+//
+//    })
+//})
